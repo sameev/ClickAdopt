070f1abd3a00314f33577a9f03030ed264d82a09
Samee Vohra <samee.vohra@gmail.com>
2022-11-02 20:27:35 -0700

    jsx refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,15 @@
-import React from "react";
-import { createRoot } from "react-dom/client"
-
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+import { createRoot } from "react-dom/client";
+import Pet from "./Pet.jsx";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      name: "Doink",
-      animal: "Cat",
-      breed: "Mix",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="Cat" breed="Mix" />
+    </div>
+  );
 };
 
 //grab the root div out of the HTML document (typically given an ID of 'root')
@@ -35,4 +17,4 @@ const container = document.getElementById("root");
 //take element above and pass it into createRoot to signal to React where we want it to render our application from
 const root = createRoot(container);
 //on root, we invoke the render method which takes in an INSTANCE of App as a parameter. (hence why we use React.createElement again)
-root.render(React.createElement(App));
+root.render(<App />);
